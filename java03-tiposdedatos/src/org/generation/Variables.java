0e0c5ee3289da1464df62f9cacea070748548cab
Michelle Garcia <michelletgar@gmail.com>
2023-05-12 15:39:48 -0600

    ci: data types

---
+++ java03-tiposdedatos/src/org/generation/Variables.java
@@ -0,0 +1,37 @@
+package org.generation;
+
+public class Variables {
+    public static void main(String[] args) {
+        /*
+        Reglas para nombre de las variables
+        - se distingue entre minusculas y mayusculas
+        - la longitud del nombre es ilimitada
+        - pueds utilizar cualquier caracter unicode
+        - la variable puede comenzar :
+          > una letra
+          > signo de dolar $
+          > caracter de subrayado
+         */
+
+        /*
+        En java tendremos los siguientes tipo de variables:
+        Varibles de instancia (non static fields)
+        Variables de clase ( static fields)
+        varibles locales ( local variables)
+        parametrso (parameters)
+        */
+
+        /*
+        * Tipos de datos:
+        * byte
+        * short
+        * int
+        * long
+        * boolean
+        * char
+        * float
+        * double
+        * */
+
+    }
+}
